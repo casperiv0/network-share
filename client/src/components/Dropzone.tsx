ffc394d510d8425edb168be757ef139e8df0ef20
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-09-01 13:24:30 +0200

    :hammer: chore: use own impl. for drop

--- client/src/components/Dropzone.tsx
+++ client/src/components/Dropzone.tsx
@@ -1,17 +1,37 @@
 import * as React from "react";
-import { FileDrop } from "react-file-drop";
 import { upload } from "lib/upload";
 
 export const Dropzone = () => {
-  async function handleDrop(files: FileList | null) {
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (!ref.current) return;
+
+    const handler = (e: Event) => e.preventDefault();
+
+    ref.current.addEventListener("drop", handler);
+    ref.current.addEventListener("dragover", handler);
+
+    return () => {
+      ref.current?.removeEventListener("drop", handler);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      ref.current?.removeEventListener("dragover", handler);
+    };
+  }, []);
+
+  async function handleDrop(event: React.DragEvent<HTMLDivElement>) {
+    event.preventDefault();
+
+    const files = event.dataTransfer.files ?? [];
+
     if (files === null) return;
 
     upload([...files]);
   }
 
   return (
-    <div style={{ height: "50vh" }}>
-      <FileDrop
+    <div ref={ref} style={{ height: "50vh" }}>
+      <div
         className="text-white w-full h-full flex items-center justify-center"
         onDrop={handleDrop}
       >
@@ -20,7 +40,7 @@ export const Dropzone = () => {
           <br />
           You will see your files upload in realtime below!
         </p>
-      </FileDrop>
+      </div>
     </div>
   );
 };
